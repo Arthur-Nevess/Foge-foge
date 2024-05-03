e11c4e5c8c7e27a2937f1cd6b3611282d8493def
Arthur-Nevess <arthurneves0a12@gmail.com>
2024-05-03 00:16:58 -0300

    Passando todas as funções para um (.c) Deichando a (main) limpa

--- main.c
+++ main.c
@@ -2,59 +2,10 @@
 # include <stdlib.h>
 # include "funções.h"
 
-  char** mapa;
-  int linhas;
-  int colunas;
-
-void ler_mapa ()
-{
-       FILE* p = fopen("mapa.txt", "r");
-    if(p==NULL)
-    {
-        puts("Falha ao abrir arquivo");
-        exit(1);
-    }
-
-    fscanf(p,"%d %d", &linhas, &colunas);
-
-     aloca_mapa();
-
-    for(int i=0;i<6;i++)
-    {
-        fscanf(p,"%s", mapa[i]);
-    }
-    fclose(p);
-}
-
- void aloca_mapa()
-{
-
-    mapa = malloc(sizeof(char*)*linhas);
-    for(int i=0;i<linhas;i++)
-    {
-        mapa[i] = malloc(sizeof(char)*(colunas+1));
-    }
-
-}
-
-    void libera()
-    {
-        for (int i=0;i<linhas;i++)
-        {
-            free(mapa[i]);
-        }
-        free(mapa);
-    }
-
 int main()
 {
     ler_mapa();
     
-    for(int i=0;i<6;i++)
-    {
-        printf("%s\n", mapa[i]);
-    }
-
     libera();
     return 0;
 }
